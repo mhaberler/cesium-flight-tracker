ae33c4c82774176e6506d13660a13e7dcd1d6ba6
Michael Haberler <git@mah.priv.at>
2023-06-21 06:41:57 +0200

    before rewrite server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { Server } = require("socket.io");
 const app = express();
-const aprs = require("aprs-parser");
 const fs = require("fs");
 var net = require('net');
 require('dotenv').config();
@@ -9,20 +8,14 @@ require('dotenv').config();
 const http = require("http");
 const host = '0.0.0.0';
 const port = 4711;
-var tracked_callsign = process.env.VUE_APP_TRACKED_CALLSIGN;
 
 const requestListener = function (req, res) {
-  // console.log('requestListener: '); // + JSON.stringify(req));
-  // console.dir(req.body);
-
   if (req.method == 'POST') {
     var body = ''
     req.on('data', function (data) {
       body += data
     })
     req.on('end', function () {
-      // console.log('Body: ' + JSON.stringify(body, null, 2))
-      // console.log('Body: ' + body);
       io.emit('sensorlogger', body); // Push to webclient
     })
   }
@@ -34,132 +27,17 @@ const httpserver = http.createServer(requestListener);
 httpserver.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
-
-
-console.log("Desired callsign target: " + tracked_callsign);
-
-var gnuradio_server = net.createServer();
-//emitted when server closes ...not emitted until all connections closes.
-gnuradio_server.on('close', function () {
-  console.log('GNURadio server closed !');
+httpserver.on('connect', function (req, socket, head) {
+  console.log('sensorlogger connect');
+});
+httpserver.on('disconnect', function (req, socket, head) {
+  console.log('sensorlogger disconnect');
 });
 
-var duplicate_preventer_timestamp_ms = 0;
-var buffer_time_ms = 7000; // To prevent duplicate packets from digipeaters. We expect new packets every 15 sec.
-// emitted when new client connects
-gnuradio_server.on('connection', function (socket) {
-  socket.setEncoding("utf8");
-  var rport = socket.remotePort;
-  var raddr = socket.remoteAddress;
-  var rfamily = socket.remoteFamily;
 
-  console.log('REMOTE Socket port: ' + rport);
-  console.log('REMOTE Socket ip : ' + raddr);
-  console.log('REMOTE Socket is IP4/IP6 : ' + rfamily);
-
-  console.log('--------------------------------------------')
-
-  socket.on('data', function (new_flight_data) {
-    const parser = new aprs.APRSParser();
-    var flight_data_json = parser.parse(new_flight_data);
-    if (flight_data_json.from == null ||
-      flight_data_json.data == null ||
-      flight_data_json.data.latitude == null ||
-      flight_data_json.data.longitude == null ||
-      flight_data_json.data.altitude == null) {
-      // ARPS without a callsign origin, latitude, longitude and altitude to be discarded.
-      console.log("\x1b[31m", "ERROR Unparsable data: " + new_flight_data, "\x1b[37m"); // output in red
-      return;
-    }
-    var callsign = flight_data_json.from.call;
-    if (callsign != tracked_callsign) {
-      console.log("\x1b[33m%s\x1b[0m", "WARNING Undesired data: " + new_flight_data, "\x1b[37m"); // output in yellow
-      return;
-    }
-    if (Date.now() - duplicate_preventer_timestamp_ms < buffer_time_ms) {
-      // Likely to be a digipeater
-      console.log("\x1b[33m%s\x1b[0m", "Likely repeated packet. Ignoring: " + new_flight_data, "\x1b[37m"); // output in yellow
-      return;
-    }
-    duplicate_preventer_timestamp_ms = Date.now();
-    var latitude = flight_data_json.data.latitude;
-    var longitude = flight_data_json.data.longitude;
-    var altitude = flight_data_json.data.altitude; // in meters
-    var timestamp = flight_data_json.data.timestamp;
-    if (timestamp == null) { // use system time received if timestamp is not present
-      timestamp = new Date().toISOString();
-    }
-    var comment = flight_data_json.data.comment;
-    var json_string = JSON.stringify({ "callsign": callsign, "longitude": longitude, "latitude": latitude, "height": altitude, "time": timestamp, "comment": comment });
-    console.log("\x1b[32m", "Parsed  " + json_string, "\x1b[37m\n"); // output in green
-    io.emit('new_flight_point', { "callsign": callsign, "longitude": longitude, "latitude": latitude, "height": altitude, "time": timestamp, "comment": comment }); // Push to webclient
-    fs.appendFileSync("flight_data.json", json_string + '\n'); // Push to database
-  });
-
-  socket.on('drain', function () {
-    console.log('write buffer is empty now .. u can resume the writable stream');
-    socket.resume();
-  });
-
-  socket.on('error', function (error) {
-    console.log('Error : ' + error);
-  });
-
-  socket.on('timeout', function () {
-    console.log('Socket timed out !');
-    socket.end('Timed out!');
-    // can call socket.destroy() here too.
-  });
-
-  socket.on('end', function (data) {
-    console.log('Socket ended from other end!');
-    console.log('End data : ' + data);
-  });
-
-  socket.on('close', function (error) {
-    var bread = socket.bytesRead;
-    var bwrite = socket.bytesWritten;
-    console.log('Bytes read : ' + bread);
-    console.log('Bytes written : ' + bwrite);
-    console.log('Socket closed!');
-    if (error) {
-      console.log('Socket was closed coz of transmission error');
-    }
-  });
-
-}); // end of gnuradio_server on connection
-
-// emits when any error occurs -> calls closed event immediately after this.
-gnuradio_server.on('error', function (error) {
-  console.log('Error: ' + error);
-});
-
-//emits when server is bound with server.listen
-gnuradio_server.on('listening', function () {
-  console.log('Server is listening!');
-});
-
-//static port allocation
-gnuradio_server.listen(3000, '0.0.0.0');
-
-var islistening = gnuradio_server.listening;
-
-if (islistening) {
-  console.log('Server is listening');
-} else {
-  console.log('Server is not listening');
-}
-
-
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-/*app.use(
-  webpackDevMiddleware(compiler)
-);*/
 app.use(require('morgan')('dev'));
 app.use(express.static('dist'))
 
-// Serve the files on port 8000.
 let server = app.listen(8000, function () {
   console.log('Cesium flight tracker app listening on port 8000!\n');
 });
@@ -168,12 +46,13 @@ const io = new Server(server);
 
 
 io.on('connection', (socket) => {
-  console.log('connection')
+  console.log('server: cesium client connect')
+  // replay history on reload
   // var prev_flight_data = fs.readFileSync("flight_data.json", 'utf-8');
   // socket.emit("prev_flight_data", prev_flight_data);
 })
 
 setInterval(() => {
-  io.emit('message', 'message')
-}, 1000)
+  io.emit('keepalive', {});
+}, 5000);
 
--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -15,10 +15,6 @@ export default {
   mounted: () => {
     const socket = io(window.location.host, { path: "/socket.io" });
 
-    socket.on("connect", () => {
-      console.log("connect");
-    });
-
     Cesium.Ion.defaultAccessToken = process.env.VUE_APP_ACCESS_TOKEN;
 
     var viewer = new Cesium.Viewer("cesiumContainer", {
@@ -27,11 +23,8 @@ export default {
         requestVertexNormals: true
       }),
       scene3DOnly: false, // Enable 2D and Columbus View
-      // timeline: false,
-      // automaticallyTrackDataSourceClocks: false,
     });
 
-    //viewer.scene.primitives.add(Cesium.createOsmBuildings());
     // The SampledPositionedProperty stores the position and timestamp for each sample along the radar sample series.
     const positionProperty = new Cesium.SampledPositionProperty();
     positionProperty.forwardExtrapolationType = Cesium.ExtrapolationType.EXTRAPOLATE;
@@ -60,88 +53,17 @@ export default {
     var loc_count = 0;
     var hpr = true;
 
-    // On fresh reload, get existing flight data from the server to plot
-    // socket.on("prev_flight_data", prev_flight_points => {
-    //   if (prev_flight_points == null) return;
-    //   prev_flight_points = prev_flight_points.split("\n");
-    //   for (var i = 0; i < prev_flight_points.length; i++) {
-    //     if (prev_flight_points[i] == "") continue; // Ignore empty strings from the delimiter split
-    //     if (start_time == null)
-    //       start_time = Cesium.JulianDate.fromIso8601(
-    //         JSON.parse(prev_flight_points[i]).time
-    //       ); // Take the first flight data point as the start point. Assumes flight data is in order.
-    //     current_time = Cesium.JulianDate.fromIso8601(
-    //       JSON.parse(prev_flight_points[i]).time
-    //     ); // Update the current time until we reach the last timestamp
-    //     var flight_point = JSON.parse(prev_flight_points[i]);
-    //     if (flight_point.height > 100000) flight_point.height = 0;
-    //     console.log(i, ": ", flight_point);
-    //     // Declare the time for this individual sample and store it in a new JulianDate instance.
-    //     const time = Cesium.JulianDate.fromIso8601(flight_point.time); // Assumes timestamp is in Iso8601.
-    //     const position = Cesium.Cartesian3.fromDegrees(
-    //       flight_point.longitude,
-    //       flight_point.latitude,
-    //       flight_point.height
-    //     );
-    //     // Store the position along with its timestamp.
-    //     // var comment = flight_point.comment.slice(0,-16); // Edit if necessary for your specific mission
-    //     // Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-    //     positionProperty.addSample(time, position);
-    //     viewer.entities.add({
-    //       description: `Location: (${flight_point.longitude}, ${flight_point.latitude}, ${flight_point.height})`,
-    //       label: {
-    //         text:
-    //           "Time: " +
-    //           time +
-    //           "; Alt: " +
-    //           Math.round(flight_point.height) +
-    //           " m",
-    //         font: "14pt monospace",
-    //         style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-    //         outlineWidth: 2,
-    //         verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-    //         pixelOffset: new Cesium.Cartesian2(0, -9),
-    //         scaleByDistance: new Cesium.NearFarScalar(0, 1, 10000, 0.0)
-    //       },
-    //       position: position,
-    //       point: { pixelSize: 10, color: Cesium.Color.RED }
-    //     });
-    //   }
-    //   if (start_time == null || current_time == null) return; // For the case that there is no previous flight data
-    //   viewer.clock.currentTime = current_time.clone(); // current_time should be the latest packet reported. Set current_time before we add a 1sec delay to the stop_time.
-    //   stop_time = Cesium.JulianDate.addSeconds(
-    //     current_time,
-    //     1,
-    //     new Cesium.JulianDate()
-    //   ); // if we only have 1 data point, stop_time > start_time
-    //   console.log("Clock interval: " + start_time + " to " + stop_time);
-    //   viewer.clock.startTime = start_time.clone();
-    //   viewer.clock.stopTime = stop_time.clone();
-    //   viewer.timeline.zoomTo(start_time, stop_time);
-    //   viewer.clock.shouldAnimate = false;
-    //   balloonEntity = viewer.entities.add({
-    //     // add path for flight points we already know.
-    //     availability: new Cesium.TimeIntervalCollection([
-    //       new Cesium.TimeInterval({ start: start_time, stop: stop_time })
-    //     ]),
-    //     position: positionProperty,
-    //     // point: { pixelSize: 30, color: Cesium.Color.GREEN },
-    //     model: { uri: "./CesiumBalloon.glb", maximumScale: 5000 },
-    //     // model: { uri: balloonUri, maximumScale: 5000},
-    //     orientation: new Cesium.VelocityOrientationProperty(positionProperty),
-    //     path: new Cesium.PathGraphics({ width: 3 })
-    //   });
-    //   viewer.trackedEntity = undefined;
-    //   // Make the camera fly to the balloon.
-    //   viewer.flyTo(balloonEntity);
-    // });
+    socket.on("connect", () => {
+      console.log("client: connected");
+    });
 
-    // \"messageId\":78,\"sessionId\":\"ff695557-89c7-4cc2-ac5b-02bda569e900\",\"deviceId\":\"1d6b3d72-643c-433c-b622-2a62cfbe7afe\",\"payload\":[{\"name\":\"orientation\",\"values\":{\"qz\":-0.9200320243835449,\"qy\":-0.3867120146751404,\"qx\":0.06201799958944321,\"qw\":0.012174000032246113,\"roll\":0.14990438520908356,\"pitch\":-0.7938886284828186,\"yaw\":-3.105118751525879},\"accuracy\":3,\"time\":1687242931159515600},{\"values\":{\"bearingAccuracy\":0,\"speedAccuracy\":0,\"verticalAccuracy\":1.7729943990707397,\"horizontalAccuracy\":20.899999618530273,\"speed\":0,\"bearing\":0,\"altitude\":316.8999938964844,\"longitude\":16.3196549,\"latitude\":48.2414312},\"name\":\"location\",\"time\":1687242931275000000},{\"name\":\"barometer\",\"values\":{\"relativeAltitude\":0.4253997802734375,\"pressure\":986.10888671875},\"accuracy\":3,\"time\":1687242932118783000},{\"name\":\"orientation\",\"values\":{\"qz\":-0.9289849996566772,\"qy\":-0.3678100109100342,\"qx\":0.03742999956011772,\"qw\":0.017311999574303627,\"roll\":0.07802247256040573,\"pitch\":-0.7541589736938477,\"yaw\":3.1352388858795166},\"accuracy\":3,\"time\":1687242932238684700}]}"
+    socket.on("keepalive", () => {
+      console.log('client: got keepalive');
+    });
 
     socket.on("sensorlogger", body => {
       // body is POSTed sensorlogger push data
       const msg = JSON.parse(body);
-      // console.log('Body: ' + JSON.stringify(msg, null, 2));
       msg.payload.forEach(p => {
         current_time = Cesium.JulianDate.fromDate(new Date(p["time"] / 1000000));
         // stop_time = Cesium.JulianDate.addSeconds(
@@ -150,12 +72,6 @@ export default {
         //   new Cesium.JulianDate()
         // );
         if (p.name == "orientation") {
-          // if (hpr) {
-          //   var hpRoll = new Cesium.HeadingPitchRoll(p.values.heading, p.values.pitch, p.values.roll);
-          //   var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpRoll);
-          //  } else {
-          //  }
-
           if (hpr) {
             if (position != null) {
               let hpr = new Cesium.HeadingPitchRoll(p.values.heading, p.values.pitch, p.values.roll);
@@ -167,10 +83,7 @@ export default {
             orientation = new Cesium.Quaternion(p.values.qx, p.values.qy, p.values.qz, p.values.qw);
             Cesium.Quaternion.normalize(orientation, norm_orient);
             orientationProperty.addSample(current_time, norm_orient);
-
           }
-
-
           console.log("orientation");
 
           // Extend the life of the balloon object
@@ -264,88 +177,6 @@ export default {
       });
     });
 
-    // New flight point received from the server
-    // socket.on("new_flight_point", new_flight_point => {
-    //   //new_flight_point is JSON obj
-    //   if (new_flight_point.height > 100000) new_flight_point.height = 0;
-    //   current_time = Cesium.JulianDate.fromIso8601(new_flight_point.time);
-    //   const position = Cesium.Cartesian3.fromDegrees(
-    //     new_flight_point.longitude,
-    //     new_flight_point.latitude,
-    //     new_flight_point.height
-    //   );
-    //   // Store the position along with its timestamp.
-    //   // var comment = new_flight_point.comment.slice(0,-16); // Edit if necessary for your specific mission
-    //   // Add at run-time as samples are received from a server.
-    //   positionProperty.addSample(current_time, position);
-    //   // Plot it on the cesium app
-    //   viewer.entities.add({
-    //     description: `Location: (${new_flight_point.longitude}, ${new_flight_point.latitude}, ${new_flight_point.height})`,
-    //     label: {
-    //       text:
-    //         "Time: " +
-    //         current_time +
-    //         "; Alt: " +
-    //         Math.round(new_flight_point.height) +
-    //         " m",
-    //       font: "14pt monospace",
-    //       style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-    //       outlineWidth: 2,
-    //       verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-    //       pixelOffset: new Cesium.Cartesian2(0, -9),
-    //       scaleByDistance: new Cesium.NearFarScalar(0, 1, 10000, 0.0)
-    //     },
-    //     position: position,
-    //     point: { pixelSize: 10, color: Cesium.Color.RED }
-    //   });
-    //   if (balloonEntity != null) {
-    //     // Where there are data already present in Cesium before new data is entered.
-    //     var revd_pkt_stop_time = Cesium.JulianDate.addSeconds(
-    //       current_time,
-    //       1,
-    //       new Cesium.JulianDate()
-    //     ); // Required for stop_time > start_time
-    //     if (Cesium.JulianDate.compare(revd_pkt_stop_time, stop_time) > 0) {
-    //       stop_time = revd_pkt_stop_time.clone(); // Incase flight data is sent in the wrong order.
-    //     }
-    //     balloonEntity.position = positionProperty; // Update the path of the balloon object
-    //     balloonEntity.orientation = new Cesium.VelocityOrientationProperty(
-    //       positionProperty
-    //     );
-    //     // Extend the life of the balloon object
-    //     balloonEntity.availability = new Cesium.TimeIntervalCollection([
-    //       new Cesium.TimeInterval({ start: start_time, stop: stop_time })
-    //     ]);
-    //     // window.alert("New packet received! Teleporting you over!");
-    //     viewer.clock.shouldAnimate = false;
-    //   } else {
-    //     // For fresh servers without any previous flight data. ie balloonEntity == null && start_time == null.
-    //     stop_time = Cesium.JulianDate.addSeconds(
-    //       current_time,
-    //       1,
-    //       new Cesium.JulianDate()
-    //     ); // Required for stop_time > start_time
-    //     viewer.clock.startTime = current_time.clone(); // define the start time
-    //     viewer.timeline.zoomTo(current_time, stop_time); // in this special case, the current_time is also the start_time
-    //     // A balloon object must be created.
-    //     balloonEntity = viewer.entities.add({
-    //       // add path for flight points we already know.
-    //       availability: new Cesium.TimeIntervalCollection([
-    //         new Cesium.TimeInterval({ start: start_time, stop: stop_time })
-    //       ]),
-    //       // model: { uri: balloonUri, maximumScale: 5000},
-    //       orientation: new Cesium.VelocityOrientationProperty(positionProperty),
-    //       position: positionProperty,
-    //       // point: { pixelSize: 30, color: Cesium.Color.GREEN },
-    //       model: { uri: "./CesiumBalloon.glb", maximumScale: 5000 },
-    //       path: new Cesium.PathGraphics({ width: 3 })
-    //     });
-    //   }
-    //   viewer.clock.currentTime = current_time.clone(); // update the clock displayed in the GUI
-    //   viewer.clock.stopTime = stop_time.clone(); // update the stop time
-    //   viewer.trackedEntity = undefined; // Untrack any objects to make the camera flight transition smooth
-    //   /*viewer.flyTo(balloonEntity);*/ // Teleport the camera to the balloon
-    // });
   }
 };
 </script>
